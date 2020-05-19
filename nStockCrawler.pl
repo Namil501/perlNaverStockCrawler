06925eed0c7d024da7a40cb0af61dc816a7cb6ca
namil501 <0.wave73.1@gmail.com>
2020-05-19 13:13:02 +0900

    주석 삭제, 추가

--- nStockCrawler.pl
+++ nStockCrawler.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl5.22.1
+##perl이 설치된 주소로 바꿔주세요.
 
 use strict;
 use warnings;
@@ -7,10 +8,8 @@ use URI;
 
 my $stockCode = $ARGV[0];
 my $URL = "https://finance.naver.com/item/main.nhn?code=".$stockCode;
-#my $URL = "https://finance.naver.com/item/main.nhn?code=005930";
 
 my $html = scraper{
 	process 'p.no_today span.blind', "span" => 'TEXT';
-#}->scrape(URI->new("https://finance.naver.com/item/main.nhn?code=005930"));
 }->scrape(URI->new($URL));
 print $html->{span};
